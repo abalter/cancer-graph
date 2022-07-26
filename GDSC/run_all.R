2ff203b5766ff370cce9b784c44e8623014d77d5
Ariel Balter <ariel@arielbalter.com>
2022-07-26 10:12:18 +0000

    changed case in clou

--- GDSC/run_all.R
+++
@@ -1,5 +0,0 @@
-library(knitr)
-
-rmarkdown::render('gdsc_data_download.Rmd')
-rmarkdown::render('gdsc_data_harmonization.Rmd')
-rmarkdown::render('gdsc_data_upload.Rmd')
--- GDSC/run_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-Rscript -e "rmarkdown::render('gdsc_data_download.Rmd')"
-Rscript -e "rmarkdown::render('gdsc_data_harmonization.Rmd')"
-Rscript -e "rmarkdown::render('gdsc_data_upload.Rmd')"
